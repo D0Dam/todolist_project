8abf159ca1b46293dbaf388f187bd114c1c306c9
Guest <guest-1qatbq@h24879.sgb.skir.kr>
2020-12-05 19:01:42 +0900

    add todo.js

---
+++ todo.js
@@ -0,0 +1,27 @@
+document.getElementById('addToDoBtn').addEventListener('click', addList);
+
+function addList(){
+  let contents=document.querySelector('.addToDoInfo');
+  if(!contents.value) {
+    alert('내용을 입력해주세요.');
+    contents.focus();
+    return false;
+  }
+
+  let tr = document.createElement('tr');
+  let input = document.createElement('input');
+  input.setAttribute('type', 'checkbox');
+  input.setAttribute('class', 'checkBtn');
+
+  let td01 = document.createElement('td');
+  td01.appendChild(input);
+  tr.appendChild(td01);
+
+  let td02 = document.createElement('td');
+  td02.innerHTML = contents.value;
+  tr.appendChild(td02);
+
+  document.getElementById('tableBody').appendChild(tr);
+  contents.value='';
+  contents.focus();
+}
